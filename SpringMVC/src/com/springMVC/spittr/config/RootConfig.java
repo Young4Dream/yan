ec04f20fc57d5cf259864228b4be111eb31443b6
Young4Dream <250739104@qq.com>
2016-10-29 20:53:01 +0800

    编写AbstractAnnotationConfigDispatcherServletInitializer的实现类，来生成DispatcherServlet和ContextLoaderListener，用以加载相应的bean

---
+++ SpringMVC/src/com/springMVC/spittr/config/RootConfig.java
@@ -0,0 +1,7 @@
+package com.springMVC.spittr.config;
+
+import org.springframework.context.annotation.*;
+
+@Configuration
+@ComponentScan(basePackages="com.springMVC.spittr")
+public class RootConfig {}
---
+++ SpringMVC/src/com/springMVC/spittr/config/WebConfig.java
@@ -0,0 +1,37 @@
+package com.springMVC.spittr.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration//由getServletConfigClasses方法返回，定义了DispatcherServlet应用上下文中的bean
+@EnableWebMvc//启用SpringMVC
+@ComponentScan(basePackages="com.springMVC.spittr.web")//启用组件扫描
+/**
+ * 配置静态资源处理
+ * 需要手动重写configureDefaultServletHandling方法
+ */
+public class WebConfig extends WebMvcConfigurerAdapter{
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		// TODO Auto-generated method stub
+		configurer.enable();
+	}
+
+	/**
+	 * 配置jsp视图解析器
+	 */
+	@Bean
+	public ViewResolver viewResolver(){
+		InternalResourceViewResolver resolver=new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		resolver.setExposeContextBeansAsAttributes(true);
+		return resolver;
+	}
+}
