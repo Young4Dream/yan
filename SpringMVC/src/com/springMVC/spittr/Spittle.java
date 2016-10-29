9d1769954c5197879484621ea23dc19e31d91de5
Young4Dream <250739104@qq.com>
2016-10-29 21:20:32 +0800

    为spittle添加属性、setter、getter方法、有参构造方法

--- SpringMVC/src/com/springMVC/spittr/Spittle.java
+++ SpringMVC/src/com/springMVC/spittr/Spittle.java
@@ -1,5 +1,44 @@
 package com.springMVC.spittr;
 
-public class Spittle {
+import java.util.Date;
 
+public class Spittle {
+	private final long id;
+	private final String message;
+	private final Date time;
+	private Double latitude;
+	private Double longitude;
+	public Spittle(String message, Date time) {
+		this(message,time,null,null);
+	}
+	@SuppressWarnings("null")
+	public Spittle(String message, Date time, Double latitude, Double longitude) {
+		super();
+		this.id = (Long) null;
+		this.message = message;
+		this.time = time;
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+	public Double getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+	public Double getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
+	public long getId() {
+		return id;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public Date getTime() {
+		return time;
+	}
 }
