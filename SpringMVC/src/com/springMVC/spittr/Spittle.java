27bd7ab93cb7be99c1382d3ae57ec265961fdbd2
Young4Dream <250739104@qq.com>
2016-10-29 21:30:16 +0800

    重写equals和hashCode方法，不知道用意，现在暂时依葫芦画瓢

--- SpringMVC/src/com/springMVC/spittr/Spittle.java
+++ SpringMVC/src/com/springMVC/spittr/Spittle.java
@@ -2,11 +2,24 @@ package com.springMVC.spittr;
 
 import java.util.Date;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
 public class Spittle {
 	private final long id;
 	private final String message;
 	private final Date time;
 	private Double latitude;
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return EqualsBuilder.reflectionEquals(this,obj,new String[]{"id","time"});
+	}
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return HashCodeBuilder.reflectionHashCode(this,new String[]{"id","time"});
+	}
 	private Double longitude;
 	public Spittle(String message, Date time) {
 		this(message,time,null,null);
