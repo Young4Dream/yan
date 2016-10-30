df5750943f4068e03965fd343dd020b1e3891614
Young4Dream <250739104@qq.com>
2016-10-30 14:58:34 +0800

    添加SpittleController文件，用以获取Spittle类相关的数据模型，并定义view界面

---
+++ SpringMVC/src/com/springMVC/spittr/web/SpittleController.java
@@ -0,0 +1,30 @@
+package com.springMVC.spittr.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+import java.util.List;
+import java.util.Map;
+
+import com.springMVC.spittr.data.SpittleRepository;
+
+@Controller
+@RequestMapping("/spittles")
+public class SpittleController {
+	private SpittleRepository spittleRepository;
+	@Autowired
+	public SpittleController (SpittleRepository spittleRepository){
+		this.spittleRepository=spittleRepository;
+	}
+	@RequestMapping(method=GET)
+	public String spittles(Model model){
+//		model.addAttribute("spittleList",spittleRepository.findSpittle(Long.MAX_VALUE, 20));
+//		return "spittles";//java.lang.AssertionError: Model attribute 'spittleList' does not exist
+
+		model.addAttribute(spittleRepository.findSpittle(Long.MAX_VALUE, 20));
+		return "spittles";
+	}
+}
