72b86751a9ce362409597cf0f82638a629dafba3
Young4Dream <250739104@qq.com>
2016-10-30 15:00:47 +0800

    修改视图前缀，以符合真正项目的视图路径

--- SpringMVC/src/com/springMVC/spittr/config/WebConfig.java
+++ SpringMVC/src/com/springMVC/spittr/config/WebConfig.java
@@ -29,7 +29,7 @@ public class WebConfig extends WebMvcConfigurerAdapter{
 	@Bean
 	public ViewResolver viewResolver(){
 		InternalResourceViewResolver resolver=new InternalResourceViewResolver();
-		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setPrefix("/WebContent/WEB-INF/views/");
 		resolver.setSuffix(".jsp");
 		resolver.setExposeContextBeansAsAttributes(true);
 		return resolver;
