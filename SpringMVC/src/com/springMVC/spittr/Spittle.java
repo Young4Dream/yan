ba56ed4189f1adffb227561b5170f656c5476a2f
Young4Dream <250739104@qq.com>
2016-10-30 13:24:24 +0800

    修改id的类型，long-->Long，否则不能在构造函数中使用null作为参数

--- SpringMVC/src/com/springMVC/spittr/Spittle.java
+++ SpringMVC/src/com/springMVC/spittr/Spittle.java
@@ -4,30 +4,20 @@ import java.util.Date;
 
 import org.apache.commons.lang.builder.EqualsBuilder;
 import org.apache.commons.lang.builder.HashCodeBuilder;
-
+import org.springframework.stereotype.Component;
+@Component
 public class Spittle {
-	private final long id;
+	private final Long id;
 	private final String message;
 	private final Date time;
 	private Double latitude;
-	@Override
-	public boolean equals(Object obj) {
-		// TODO Auto-generated method stub
-		return EqualsBuilder.reflectionEquals(this,obj,new String[]{"id","time"});
-	}
-	@Override
-	public int hashCode() {
-		// TODO Auto-generated method stub
-		return HashCodeBuilder.reflectionHashCode(this,new String[]{"id","time"});
-	}
 	private Double longitude;
 	public Spittle(String message, Date time) {
-		this(message,time,null,null);
+		this(null,message,time,null,null);
 	}
-	@SuppressWarnings("null")
-	public Spittle(String message, Date time, Double latitude, Double longitude) {
+	public Spittle(Long id,String message, Date time, Double latitude, Double longitude) {
 		super();
-		this.id = (Long) null;
+		this.id =id;
 		this.message = message;
 		this.time = time;
 		this.latitude = latitude;
@@ -35,6 +25,11 @@ public class Spittle {
 	}
 	public Double getLatitude() {
 		return latitude;
+	}
+	@Override
+	public String toString() {
+		return "Spittle [id=" + id + ", message=" + message + ", time=" + time + ", latitude=" + latitude
+				+ ", longitude=" + longitude + "]";
 	}
 	public void setLatitude(Double latitude) {
 		this.latitude = latitude;
@@ -53,5 +48,13 @@ public class Spittle {
 	}
 	public Date getTime() {
 		return time;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		return EqualsBuilder.reflectionEquals(this,obj,new String[]{"id","time"});
+	}
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this,new String[]{"id","time"});
 	}
 }
