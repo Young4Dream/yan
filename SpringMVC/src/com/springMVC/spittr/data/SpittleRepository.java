b81eaa530b91359bbb664e2dc93e2e5d5ed0a0ce
Young4Dream <250739104@qq.com>
2016-10-30 13:26:58 +0800

    为程序提供接口，并定义List类型方法，目的是返回Spittle类型的集合

---
+++ SpringMVC/src/com/springMVC/spittr/data/SpittleRepository.java
@@ -0,0 +1,9 @@
+package com.springMVC.spittr.data;
+
+import java.util.List;
+
+import com.springMVC.spittr.Spittle;
+
+public interface SpittleRepository {
+	List<Spittle> findSpittle(long max,int count);
+}
