72cc2d4333ebf499b801d398c1c851c7a690d369
Young4Dream <250739104@qq.com>
2016-10-30 18:05:48 +0800

    添加jsp页面和web.xml

---
+++ SpringMVC/src/com/springMVC/spittr/data/SpittleRepositoryImpl.java
@@ -0,0 +1,22 @@
+package com.springMVC.spittr.data;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.springMVC.spittr.Spittle;
+@Component
+public class SpittleRepositoryImpl implements SpittleRepository{
+
+	@Override
+	public List<Spittle> findSpittle(long max, int count) {
+		List<Spittle> spittleList=new ArrayList<>();
+		for(int k=0;k<count;k++){
+			spittleList.add(new Spittle("spittle"+k,new Date()));
+		}
+		return spittleList;
+	}
+
+}
