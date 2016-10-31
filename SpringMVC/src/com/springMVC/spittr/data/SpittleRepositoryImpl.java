a9d3301b7832f85645e57c9be1003a12f17b131d
Yan Wei <250739104@qq.com>
2016-10-31 17:52:28 +0800

    兼容MyEclipse

--- SpringMVC/src/com/springMVC/spittr/data/SpittleRepositoryImpl.java
+++ SpringMVC/src/com/springMVC/spittr/data/SpittleRepositoryImpl.java
@@ -1,9 +1,11 @@
 package com.springMVC.spittr.data;
 
+import java.text.ParseException;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
 import org.springframework.stereotype.Component;
 
 import com.springMVC.spittr.Spittle;
@@ -13,7 +15,16 @@ public class SpittleRepositoryImpl implements SpittleRepository{
 	public List<Spittle> findSpittle(long max, int count) {
 		List<Spittle> spittleList=new ArrayList<Spittle>();
 		for(int k=0;k<count;k++){
-			spittleList.add(new Spittle("spittle"+k,new Date()));
+			  Date currentTime = new Date();
+			  SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+			  String dateString = formatter.format(currentTime);
+			  try {
+				  Date currentTime_2 = formatter.parse(dateString);
+				spittleList.add(new Spittle("spittle"+k,currentTime_2));
+			} catch (ParseException e) {
+				e.printStackTrace();
+				System.err.println("errr");
+			}
 		}
 		return spittleList;
 	}
