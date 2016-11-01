89c06a633843c70f86b62cab114bf3a787730d63
Young4Dream <250739104@qq.com>
2016-11-01 22:08:17 +0800

    初始化配置类的类，继承AbstractAnnotationConfigDispatcherServletInitializer类并重写其方法

--- SpringMVC/spittr/src/com/spring/mvc/config/WebAppInitializer.java
+++ SpringMVC/spittr/src/com/spring/mvc/config/WebAppInitializer.java
@@ -1,5 +1,22 @@
 package com.spring.mvc.config;
 
-public class WebAppInitializer {
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[]{RootConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[]{WebConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[]{"/"};
+	}
 
 }
