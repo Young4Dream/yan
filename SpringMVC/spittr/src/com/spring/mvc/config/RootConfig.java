6cb86872b7b8ad55e1bc65bfafd7331cf6e82044
Yan Wei <250739104@qq.com>
2016-11-01 10:35:35 +0800

    Rebuild the spittr cause import problem...

---
+++ SpringMVC/spittr/src/com/spring/mvc/config/RootConfig.java
@@ -0,0 +1,5 @@
+package com.spring.mvc.config;
+
+public class RootConfig {
+
+}
--- SpringMVC/src/com/springMVC/spittr/data/SpittleRepositoryImpl.java
+++ SpringMVC/src/com/springMVC/spittr/data/SpittleRepositoryImpl.java
@@ -1,13 +1,9 @@
 package com.springMVC.spittr.data;
 
-import java.text.ParseException;
-import java.text.ParsePosition;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import org.aspectj.apache.bcel.verifier.statics.LONG_Upper;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -15,7 +11,7 @@ import com.springMVC.spittr.Spittle;
 @Component
 public class SpittleRepositoryImpl implements SpittleRepository{
 	public static final String MAX_LONG_AS_STRING= Long.toString(Long.MAX_VALUE);
-	public List<Spittle> findSpittle(@RequestParam(value="max",defaultValue=MAX_LONG_AS_STRING)long max,
+	public List<Spittle> findSpittle(@RequestParam(value="max",defaultValue="9999")long max,
 				@RequestParam(value="count",defaultValue="5")int count) {
 		List<Spittle> spittleList=new ArrayList<Spittle>();
 		for(int k=0;k<count;k++){
