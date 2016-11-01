193ed27b5cc7878f901ea20a5c3f213d6757a153
Yan Wei <250739104@qq.com>
2016-11-01 17:54:06 +0800

    no big change

--- SpringMVC/spittr/src/com/spring/mvc/config/RootConfig.java
+++ SpringMVC/spittr/src/com/spring/mvc/config/RootConfig.java
@@ -1,5 +1,17 @@
 package com.spring.mvc.config;
 
-public class RootConfig {
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.ImportResource;
 
-}
+/**
+ * 根配置，只需要标明Configuration和ComponentScan注解即可
+ * @author yan
+ *
+ */
+@Configuration
+@ComponentScan("com.spring")//扫描com.spring中所有带有Component或repository等标记的实现类，用以生成bean
+@EnableAspectJAutoProxy//使AOP生效
+@ImportResource("classpath:/applicationContext.xml")
+public class RootConfig {}
---
+++ SpringMVC/spittr/src/com/spring/mvc/config/WebConfig.java
@@ -0,0 +1,32 @@
+package com.spring.mvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+/**
+ * @author yan
+ * 1.是配置类，相当于xml文件，故用注解@Configuration标明；
+ * 2.使用注解@EnableWebMvc表示用MVC方式处理web请求
+ * 3.需要手动重写configureDefaultServletHandling方法
+ *
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter{
+	/**
+	 * 配置jsp视图解析器
+	 */
+	@Bean
+	public ViewResolver viewResolver(){
+		InternalResourceViewResolver resolver=new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		resolver.setExposeContextBeansAsAttributes(true);
+		return resolver;
+	}
+}
