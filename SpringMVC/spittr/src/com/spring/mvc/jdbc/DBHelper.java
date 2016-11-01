844f6a21fcc9d1579cfe4e9424522a77bd3f76e1
Young4Dream <250739104@qq.com>
2016-11-01 23:06:07 +0800

    1.定义DataSource 2.获取连接 3.定义JDBCTemplate 4.定义NamedParameterJDBCTemplate

--- SpringMVC/spittr/src/com/spring/mvc/jdbc/DBHelper.java
+++ SpringMVC/spittr/src/com/spring/mvc/jdbc/DBHelper.java
@@ -1,15 +1,28 @@
 package com.spring.mvc.jdbc;
 
+import java.sql.SQLException;
+
 import javax.sql.DataSource;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 public class DBHelper{
 	public static ApplicationContext ctx=null;
 	public static DataSource ds=null;
+	public static JdbcTemplate jt=null;
+	public static NamedParameterJdbcTemplate npjt=null;
 	{
-		ctx=new ClassPathXmlApplicationContext("applicationContext.xml");
-		ds=(DataSource) ctx.getBean(javax.activation.DataSource.class);
+		try {
+			ctx=new ClassPathXmlApplicationContext("applicationContext.xml");
+			ds=(DataSource) ctx.getBean(javax.activation.DataSource.class);
+			ds.getConnection();
+			jt=(JdbcTemplate) ctx.getBean("jdbcTemplate");
+			npjt=(NamedParameterJdbcTemplate) ctx.getBean(NamedParameterJdbcTemplate.class);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
