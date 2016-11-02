84f270bfb26f4081ca3b4930a7465d6144153eb3
Yan Wei <250739104@qq.com>
2016-11-02 17:15:19 +0800

    新增controller

---
+++ SpringMVC/spittr/src/com/spring/mvc/controller/SpittleController.java
@@ -0,0 +1,23 @@
+package com.spring.mvc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+//import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import com.spring.mvc.dao.SpittleDao;
+
+@Controller
+@RequestMapping("/")
+public class SpittleController {
+	@Autowired
+	private SpittleDao spittleDao;
+	@RequestMapping//(value="spittles",method=GET)
+	public ModelAndView spittles(){
+		ModelAndView mv=new ModelAndView();
+		Object o=spittleDao.findAll();
+		mv.addObject("spittlesList", o);
+		mv.setViewName("spittles");
+		return mv;
+	}
+}
