32ad8a7eadb270eb52d5baaabf0cfdd2fcfae79a
Yan Wei <250739104@qq.com>
2016-11-02 14:40:13 +0800

    1.将applicationcontext、JDBCTemplate封装成方法，applicationcontext设置成spring启动时自动获取； 2.定义getConnection方法，返回连接

--- SpringMVC/spittr/src/com/spring/mvc/jdbc/DBHelper.java
+++ SpringMVC/spittr/src/com/spring/mvc/jdbc/DBHelper.java
@@ -1,28 +1,41 @@
 package com.spring.mvc.jdbc;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
-public class DBHelper{
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public class DBHelper implements ApplicationContextAware{
 	public static ApplicationContext ctx=null;
 	public static DataSource ds=null;
 	public static JdbcTemplate jt=null;
 	public static NamedParameterJdbcTemplate npjt=null;
-	{
+	/**
+	 * 在Spring启动时自动获得Spring 的 ApplicationContext
+	 */
+	public void setApplicationContext(ApplicationContext arg0)
+			throws BeansException {
+		DBHelper.ctx=arg0;
+		jt=(JdbcTemplate) ctx.getBean("jdbcTemplate");
+		npjt=(NamedParameterJdbcTemplate) ctx.getBean(NamedParameterJdbcTemplate.class);
+	}
+	/**
+	 * 根据ctx对象获取连接
+	 */
+	public static synchronized Connection getConnection(){
 		try {
-			ctx=new ClassPathXmlApplicationContext("applicationContext.xml");
-			ds=(DataSource) ctx.getBean(javax.activation.DataSource.class);
-			ds.getConnection();
-			jt=(JdbcTemplate) ctx.getBean("jdbcTemplate");
-			npjt=(NamedParameterJdbcTemplate) ctx.getBean(NamedParameterJdbcTemplate.class);
+			ComboPooledDataSource cds=(ComboPooledDataSource) ctx.getBean("dataSource");
+			return cds.getConnection();
 		} catch (SQLException e) {
-			e.printStackTrace();
+			return null;
 		}
 	}
 }
