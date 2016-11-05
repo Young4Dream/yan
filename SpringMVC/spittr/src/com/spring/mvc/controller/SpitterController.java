7ca668110c9fa2a389219ddd79d15d997943cb24
Young4Dream <250739104@qq.com>
2016-11-05 21:42:22 +0800

    删除注册Spitter时接收参数的@RequestParam注解，否则无法将其封装成Spitter类

--- SpringMVC/spittr/src/com/spring/mvc/controller/SpitterController.java
+++ SpringMVC/spittr/src/com/spring/mvc/controller/SpitterController.java
@@ -1,8 +1,12 @@
 package com.spring.mvc.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,12 +34,28 @@ public class SpitterController {
 	 * @return
 	 */
 	@RequestMapping(value="/register",method=RequestMethod.POST)
-	public String register(@RequestParam("firstName") String firstName,@RequestParam String lastName,@RequestParam String email,@RequestParam String username,@RequestParam String password){
-		Spitter s=new Spitter(firstName,lastName,email,username,password);
-		System.out.println(s);
-		spitterDaoImpl.add(s);
-		return "redirect:/spitter/"+s.getUsername();
+	public String register(@Valid Spitter spitter,Errors errors){
+		if(errors.hasErrors()){
+			return "registerForm";
+		}
+		System.out.println(spitter);
+		spitterDaoImpl.add(spitter);
+		return "redirect:/spitter/"+spitter.getUsername();
 	}
+//	@RequestMapping(value="/register",method=RequestMethod.POST)
+//	public String register(@RequestParam("firstName") @Valid String firstName,Errors error,
+//			@RequestParam  @Valid String lastName, Errors e,
+//			@Valid @RequestParam String email,Errors err,
+//			@RequestParam @Valid String username,Errors errs,
+//			@RequestParam @Valid String password,Errors error1){
+//		if(e.hasErrors()){
+//			return "registerForm";
+//		}
+//		Spitter s=new Spitter(firstName,lastName,email,username,password);
+//		System.out.println(s);
+//		spitterDaoImpl.add(s);
+//		return "redirect:/spitter/"+s.getUsername();
+//	}
 	/**
 	 * 获取个人信息
 	 * @param username
