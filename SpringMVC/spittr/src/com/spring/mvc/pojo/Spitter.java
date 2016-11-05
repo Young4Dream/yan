502faf36493e457659aa979a43b5975b5f5f6d8c
Young4Dream <250739104@qq.com>
2016-11-05 21:41:02 +0800

    将基础类型int替换为包装类Integer

--- SpringMVC/spittr/src/com/spring/mvc/pojo/Spitter.java
+++ SpringMVC/spittr/src/com/spring/mvc/pojo/Spitter.java
@@ -1,29 +1,43 @@
 package com.spring.mvc.pojo;
 
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Spitter {
-	private int id;
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
+	private Integer id;
+	@NotNull
+	@Size(max=20,min=6)
 	private String firstName;
+	@NotNull
+	@Size(max=20,min=6)
 	private String lastName;
+	@NotNull
+	@Size(max=20,min=6)
 	private String email;
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
+	@NotNull
+	@Size(max=20,min=6)
 	private String username;
-	@Override
-	public String toString() {
-		return "Spitter [firstName=" + firstName + ", lastName=" + lastName
-				+ ", email=" + email + ", username=" + username + ", password="
-				+ password + "]";
+	@NotNull
+	@Size(max=20,min=6)
+	private String password;
+	/**
+	 * Constructor with no parameters
+	 */
+	public Spitter() {
+		super();
 	}
+	/**
+	 * Constructor with full parameters
+	 * @param firstName
+	 * @param lastName
+	 * @param email
+	 * @param username
+	 * @param password
+	 */
+	@Autowired
 	public Spitter(String firstName, String lastName, String email,
 			String username, String password) {
 		super();
@@ -33,9 +47,41 @@ public class Spitter {
 		this.username = username;
 		this.password = password;
 	}
-	private String password;
-	public Spitter() {
+	/**
+	 * Override method toString()
+	 */
+	@Override
+	public String toString() {
+		return "Spitter [firstName=" + firstName + ", lastName=" + lastName
+				+ ", email=" + email + ", username=" + username + ", password="
+				+ password + "]";
+	}
+	
+	public Spitter(Integer id, String firstName, String lastName, String email,
+			String username, String password) {
 		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.username = username;
+		this.password = password;
+	}
+	/**
+	 * set/get method
+	 * @return
+	 */
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
 	}
 	public String getFirstName() {
 		return firstName;
