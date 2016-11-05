d1adea38e69e64f9825a9c82f8c5c489b1c61401
Young4Dream <250739104@qq.com>
2016-11-05 13:36:25 +0800

    定义spitter

--- SpringMVC/spittr/src/com/spring/mvc/pojo/Spitter.java
+++ SpringMVC/spittr/src/com/spring/mvc/pojo/Spitter.java
@@ -1,8 +1,64 @@
 package com.spring.mvc.pojo;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class Spitter {
-
+	private int id;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	private String firstName;
+	private String lastName;
+	private String email;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	private String username;
+	@Override
+	public String toString() {
+		return "Spitter [firstName=" + firstName + ", lastName=" + lastName
+				+ ", email=" + email + ", username=" + username + ", password="
+				+ password + "]";
+	}
+	public Spitter(String firstName, String lastName, String email,
+			String username, String password) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.username = username;
+		this.password = password;
+	}
+	private String password;
+	public Spitter() {
+		super();
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String userName) {
+		this.username = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
