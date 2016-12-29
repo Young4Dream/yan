f4c0a130a41722d875c4227c2244a125beb44127
Yan Wei <250739104@qq.com>
2016-12-30 01:21:53 +0800

    删除多余配置

--- MyBatis/src/main/java/com/yan/dao/mapper/DeptMapper.java
+++ MyBatis/src/main/java/com/yan/dao/mapper/DeptMapper.java
@@ -8,25 +8,6 @@ import org.apache.ibatis.annotations.Param;
 public interface DeptMapper {
 	
 	Dept findEmpsByDname(String dname);
-    int countByExample(DeptExample example);
-
-    int deleteByExample(DeptExample example);
-
-    int deleteByPrimaryKey(Short deptno);
-
-    int insert(Dept record);
-
-    int insertSelective(Dept record);
-
-    List<Dept> selectByExample(DeptExample example);
 
     Dept selectByPrimaryKey(Short deptno);
-
-    int updateByExampleSelective(@Param("record") Dept record, @Param("example") DeptExample example);
-
-    int updateByExample(@Param("record") Dept record, @Param("example") DeptExample example);
-
-    int updateByPrimaryKeySelective(Dept record);
-
-    int updateByPrimaryKey(Dept record);
 }
--- MyBatis/src/main/java/com/yan/dao/mapper/EmpMapper.java
+++ MyBatis/src/main/java/com/yan/dao/mapper/EmpMapper.java
@@ -8,25 +8,6 @@ import org.apache.ibatis.annotations.Param;
 public interface EmpMapper {
 	
 	Emp findByAss(short empno);
-    int countByExample(EmpExample example);
-
-    int deleteByExample(EmpExample example);
-
-    int deleteByPrimaryKey(Short empno);
-
-    int insert(Emp record);
-
-    int insertSelective(Emp record);
-
-    List<Emp> selectByExample(EmpExample example);
-
     Emp selectByPrimaryKey(short i);
-
-    int updateByExampleSelective(@Param("record") Emp record, @Param("example") EmpExample example);
-
-    int updateByExample(@Param("record") Emp record, @Param("example") EmpExample example);
-
-    int updateByPrimaryKeySelective(Emp record);
-
-    int updateByPrimaryKey(Emp record);
+    
 }
