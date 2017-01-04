ba17584a086df74977742531f86bed4a98ff8661
Yan Wei <250739104@qq.com>
2017-01-04 13:30:12 +0800

    完成日志输出部分

--- ssm/src/main/java/com/yan/ssm/service/impl/EmpServiceImpl.java
+++ ssm/src/main/java/com/yan/ssm/service/impl/EmpServiceImpl.java
@@ -2,54 +2,57 @@ package com.yan.ssm.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.yan.ssm.dao.mapper.EmpMapper;
 import com.yan.ssm.pojo.Emp;
 import com.yan.ssm.pojo.EmpExample;
 import com.yan.ssm.service.EmpService;
 @Service
 public class EmpServiceImpl implements EmpService{
-
+	@Autowired
+EmpMapper empMapper;
 	@Override
 	public int countByExample(EmpExample example) {
 		// TODO Auto-generated method stub
-		return 0;
+		return empMapper.countByExample(example);
 	}
 
 	@Override
 	public int deleteByExample(EmpExample example) {
 		// TODO Auto-generated method stub
-		return 0;
+		return empMapper.deleteByExample(example);
 	}
 
 	@Override
 	public int deleteByPrimaryKey(Short empno) {
 		// TODO Auto-generated method stub
-		return 0;
+		return empMapper.deleteByPrimaryKey(empno);
 	}
 
 	@Override
 	public int insert(Emp record) {
 		// TODO Auto-generated method stub
-		return 0;
+		return empMapper.insert(record);
 	}
 
 	@Override
 	public int insertSelective(Emp record) {
 		// TODO Auto-generated method stub
-		return 0;
+		return empMapper.insertSelective(record);
 	}
 
 	@Override
 	public List<Emp> selectByExample(EmpExample example) {
 		// TODO Auto-generated method stub
-		return null;
+		return empMapper.selectByExample(example);
 	}
 
 	@Override
 	public Emp selectByPrimaryKey(Short empno) {
 		// TODO Auto-generated method stub
-		return null;
+		return empMapper.selectByPrimaryKey(empno);
 	}
 
 	@Override
