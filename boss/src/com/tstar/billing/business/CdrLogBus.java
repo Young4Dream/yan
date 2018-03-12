578107dcbff53ef97801efc7f6c15958103d8938
yw007 <250739104@qq.com>
2018-03-12 22:05:07 +0800

    aa

--- boss/src/com/tstar/billing/business/CdrLogBus.java
+++ boss/src/com/tstar/billing/business/CdrLogBus.java
@@ -1,55 +1,58 @@
 package com.tstar.billing.business;
 
-import java.util.Date;
-import java.util.List;
-
-import org.apache.ibatis.session.SqlSession;
-
 import com.tstar.ocs.dao.CdrLogMapper;
 import com.tstar.ocs.model.CdrLog;
 import com.tstar.util.MyBatisSessionFactory;
 import com.tstar.util.UUID;
+import org.apache.ibatis.session.SqlSession;
+
+import java.util.Date;
+import java.util.List;
 
 public class CdrLogBus {
 
-	public static List<CdrLog> getSortingLogs(int status) {
-		SqlSession session = MyBatisSessionFactory.getSession();
-		try {
-			CdrLogMapper dao = session.getMapper(CdrLogMapper.class);
-			return dao.selectByFlag(CdrLog.STATUS_INIT);
-		} finally {
-			session.close();
-		}
-	}
-	
-	public static CdrLog getLastLog(String sourceId, String fileName) {
-		SqlSession session = MyBatisSessionFactory.getSession();
-		try {
-			CdrLogMapper dao = session.getMapper(CdrLogMapper.class);
-			return dao.selectLastLog(sourceId, fileName);
-		} finally {
-			session.close();
-		}
-	}
-	
-	public static void save(CdrLog log) {
-		SqlSession session = MyBatisSessionFactory.getSession();
-		try {
-			CdrLogMapper dao = session.getMapper(CdrLogMapper.class);
-			if (log.getId() == null) {
-				log.setId(UUID.get());
-				log.setCreateTime(new Date());
-				dao.insert(log);
-			} else {
-				log.setUpdateTime(new Date());
-				dao.updateByPrimaryKey(log);
-			}
-			session.commit();
-		} finally {
-			session.close();
-		}
-	}
-	
-	public static void main(String[] args) {
-	}
+    public static List<CdrLog> getSortingLogs(int status) {
+        SqlSession session = MyBatisSessionFactory.getSession();
+        try {
+            CdrLogMapper dao = session.getMapper(CdrLogMapper.class);
+            return dao.selectByFlag(CdrLog.STATUS_INIT);
+        } finally {
+            session.close();
+        }
+    }
+
+    public static CdrLog getLastLog(String sourceId, String fileName) {
+        SqlSession session = MyBatisSessionFactory.getSession();
+        try {
+            CdrLogMapper dao = session.getMapper(CdrLogMapper.class);
+            return dao.selectLastLog(sourceId, fileName);
+        } finally {
+            session.close();
+        }
+    }
+
+    public static void save(CdrLog log) {
+        SqlSession session = MyBatisSessionFactory.getSession();
+        try {
+            CdrLogMapper dao = session.getMapper(CdrLogMapper.class);
+            if (log.getId() == null) {
+                log.setId(UUID.get());
+                log.setCreateTime(new Date());
+                dao.insert(log);
+            } else {
+                log.setUpdateTime(new Date());
+                dao.updateByPrimaryKey(log);
+            }
+            session.commit();
+        } finally {
+            session.close();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        ICallable iCallable = new ICallable("hah", "xixi", "cc");
+        Thread thread = iCallable.creatCallableThread();
+        thread.start();
+        iCallable.get();
+    }
 }
